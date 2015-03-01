79b7d8208bf5256f6a2b5982c1e7860628e630c0
Nicholas Goehner <nicholasgoehner@Nicholass-MacBook-Pro.local>
2015-03-01 13:20:34 -0800

    Initial Commit

---
+++ xcodeClass1postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xcodeClass1postcard
+//
+//  Created by Nicholas Goehner on 3/1/15.
+//  Copyright (c) 2015 Nicholas Goehner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
